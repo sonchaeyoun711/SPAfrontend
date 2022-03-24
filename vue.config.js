ed74aed898f8cf8fd43ed4dc7c42e36288beea0b
Chaeyoun Son <cy.son97@gmail.com>
2022-03-24 15:14:00 +0900

    clean ver

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,9 @@
 // 참고 : https://doozi0316.tistory.com/entry/Vuejs-Spring-Boot-MySQL-MyBatis-%EB%A7%9B%EC%A7%91-%EC%A7%80%EB%8F%84-%EB%A7%8C%EB%93%A4%EA%B8%B01-Spring-Boot-Vuejs-%EC%84%A4%EC%B9%98-%EB%B0%8F-%EC%97%B0%EB%8F%99%ED%95%98%EA%B8%B0
 module.exports = {
-  outputDir: "../src/main/resources/static",  // 빌드 타겟 디렉토리
+  outputDir: "../src/main/resources/static",  // build target dir
   devServer: {
     //port: 8080,
     //overlay: false,
-    /*proxy: {
-      '/': {
-        // '/api' 로 들어오면 포트 8081(스프링 서버)로 보낸다
-        target: 'http://localhost:8081',
-        //pathRewrite: {'/': ''},
-        logLevel: 'debug',
-        changeOrigin: true // cross origin 허용
-      }
-    }*/
     proxy: 'http://localhost:8081'
   }
 };
